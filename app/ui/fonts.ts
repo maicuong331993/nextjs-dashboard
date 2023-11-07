dd29583d481e70072c774557475090aa2e809e78
Son Nguyen <alldoggotoaheaven@gmail.com>
2023-11-07 16:49:39 +0700

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Chakra_Petch, Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400','700']});
+export const raleway = Chakra_Petch({subsets:['vietnamese'], weight:['400']})
